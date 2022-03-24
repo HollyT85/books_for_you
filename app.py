46ad01c75bbbf4229f0d7c58639d91c58538d72c
HollyT85 <h.e.turner@gmail.com>
2022-03-24 19:02:37 +0000

    set secret key

--- app.py
+++ app.py
@@ -2,12 +2,21 @@
 Imports for functionality
 """
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for
-)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
+
 if os.path.exists("env.py"):
     import env
+
+app = Flask(__name__)
+
+
+# test function
+@app.route("/")
+def hello():
+    return "Hello"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
