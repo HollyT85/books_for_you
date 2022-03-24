2710e2fd1b3891c57dce1e9864c9a812a5e44f9a
HollyT85 <h.e.turner@gmail.com>
2022-03-24 19:22:46 +0000

    addded configurations

--- app.py
+++ app.py
@@ -2,18 +2,30 @@
 Imports for functionality
 """
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
+# check for env.py file
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+# set configurations
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-# test function
+mongo = PyMongo(app)
+
+
+# home page function
 @app.route("/")
-def hello():
-    return "Hello"
+def home_page():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
