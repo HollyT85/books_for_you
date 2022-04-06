ef48a56ba0a98afe143bcc01246691e8ce6d1176
HollyT85 <h.e.turner@gmail.com>
2022-04-06 19:28:39 +0000

    finished fixing collapse errors

--- app.py
+++ app.py
@@ -176,11 +176,6 @@ def viewreview():
     return render_template("viewreview.html")
 
 
-@app.route("/collapse")
-def collapse():
-    return render_template("collapse.html")
-
-
 @app.route("/books")
 def browsebooks():
     """
